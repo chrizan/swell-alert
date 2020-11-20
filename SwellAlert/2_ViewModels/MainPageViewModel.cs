1217e541e5de80682b5cb5ebd38c81bcb6907950
Christoph Zanner <CZanner@Hamilton.ch>
2020-11-20 09:57:17 +0100

    MainPageViewModel added

---
+++ SwellAlert/2_ViewModels/MainPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace SwellAlert.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- SwellAlert/App.xaml.cs
+++ SwellAlert/App.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using SwellAlert.Views;
-using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace SwellAlert
 {
