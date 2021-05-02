0d4ec9ea508e833ada5a0bc51e1f833196b6edf8
ChriZan <christophzanner@gmx.ch>
2021-05-02 12:23:25 +0200

    Add WorkManager and Notification

--- SwellAlert/SwellAlert.Android/MainActivity.cs
+++ SwellAlert/SwellAlert.Android/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Work;
 using Prism;
 using Prism.Ioc;
+using System;
 
 namespace SwellAlert.Droid
 {
@@ -10,6 +13,10 @@ namespace SwellAlert.Droid
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static readonly int NOTIFICATION_ID = 1000;
+        static readonly string CHANNEL_ID = "location_notification";
+        internal static readonly string COUNT_KEY = "count";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -17,8 +24,14 @@ namespace SwellAlert.Droid
 
             base.OnCreate(savedInstanceState);
 
+            CreateNotificationChannel();
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App(new AndroidInitializer()));
+
+            PeriodicWorkRequest testWorkRequest = PeriodicWorkRequest.Builder.From<TestWorker>(TimeSpan.FromMinutes(16)).Build();
+
+            WorkManager.GetInstance(Xamarin.Essentials.Platform.AppContext).Enqueue(testWorkRequest);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
@@ -26,6 +39,27 @@ namespace SwellAlert.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var name = "channel_name"; //Resources.GetString(Resource.String.channel_name);
+            var description = "channel_description"; //GetString(Resource.String.channel_description);
+            var channel = new NotificationChannel(CHANNEL_ID, name, NotificationImportance.Default)
+            {
+                Description = description
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 
---
+++ SwellAlert/SwellAlert.Android/TestWorker.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using AndroidX.Core.App;
+using AndroidX.Work;
+
+namespace SwellAlert.Droid
+{
+    public class TestWorker : Worker
+    {
+        private readonly Context _context;
+        public TestWorker(Context context, WorkerParameters workerParameters) : base(context, workerParameters)
+        {
+            _context = context;
+        }
+
+        public override Result DoWork()
+        {
+            Android.Util.Log.Debug(nameof(TestWorker), $"{nameof(TestWorker)} executed");
+
+
+            // Build the notification:
+            var builder = new NotificationCompat.Builder(_context, "CHANNEL_ID")
+                          .SetAutoCancel(true) // Dismiss the notification from the notification area when the user clicks on it
+                          .SetContentTitle("Swell Alert triggered") // Set the title
+                          .SetSmallIcon(Resource.Drawable.test_icon)
+                          .SetContentText("This is the content text for the Swell Alert notification"); // the message to display.
+
+            // Finally, publish the notification:
+            var notificationManager = NotificationManagerCompat.From(_context);
+            notificationManager.Notify(333, builder.Build());
+
+            return Result.InvokeSuccess();
+        }
+    }
+}
